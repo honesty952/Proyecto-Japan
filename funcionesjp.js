3d2dae81ed337ebbf298250667ffa962a2ad46d0
Cristobal <cbcristobalbahamonde51@gmail.com>
2020-05-21 17:17:14 -0300

    Proyecto japon, base página 1 d	new file:   fonts/RobotoCondensed-Italic.ttf

---
+++ funcionesjp.js
@@ -0,0 +1,21 @@
+
+function ejecuta(){
+
+
+for (var i=0; i<3; i++){
+
+    
+    document.getElementById("li")[i].onclick=saludo;{
+        
+    }
+    
+}
+}
+
+function saludo(){
+
+    alert("hola, funciono y estoy feliz")
+
+}
+
+window.onload=ejecuta;
